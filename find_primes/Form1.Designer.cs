757042c54d44ce52374ff50146472beabbedff18
SmoxBoye <simon.sbg@gmail.com>
2017-10-22 20:39:09 +0200

    A simple program to find all prime numbers from 0 to the entered value. (instructions are written in swedish but just hit the button lol)

---
+++ find_primes/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace find_primes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(0, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 104);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kör";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(0, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(402, 12);
+            this.textBox2.MinimumSize = new System.Drawing.Size(376, 506);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(376, 506);
+            this.textBox2.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.progressBar1.Location = new System.Drawing.Point(0, 178);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(282, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(60, 277);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(282, 217);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Skriv det största talet du vill testa i \r\ntextrunan nedan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 530);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(806, 568);
+            this.Name = "Form1";
+            this.Text = "Simons Primtal Generator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ find_primes/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace find_primes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            List<int> listPrimes = new List<int>();
+
+            int intMaxP = 0;
+            try
+            {
+                intMaxP = int.Parse(textBox1.Text);
+            }
+            catch
+            {
+                intMaxP = 0;
+            }
+
+
+            if (intMaxP > 0)
+            {
+                intMaxP = intMaxP + 1;
+                string addText = "";
+                textBox2.Text = "2 ";
+                int aproxPrime = (int)(intMaxP / Math.Log(intMaxP));
+                progressBar1.Maximum = aproxPrime + 1;
+
+
+                for (int i = 3; i < intMaxP; i = i + 2)
+                {
+                    bool isPrime = true;
+                    int sqPrime = (int)(Math.Sqrt(i));
+                    int x = 0;
+                    while (x < listPrimes.Count && listPrimes[x] <= sqPrime)
+                    {
+
+                        if (i % listPrimes[x] == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+
+                        x++;
+                    }
+                    if (isPrime)
+                    {
+                        listPrimes.Add(i);
+                        addText = i.ToString();
+                        textBox2.AppendText(addText + " ");
+                        if (progressBar1.Maximum >= listPrimes.Count)
+                        {
+                            progressBar1.Value = listPrimes.Count;
+                        }
+                    }
+
+                }
+                
+                progressBar1.Value = progressBar1.Maximum;
+                MessageBox.Show("Klar");
+            }
+            else
+            {
+                MessageBox.Show(textBox1.Text + " är inte ett tillåtet värde. Använd ett positivt heltal större än 1");
+            }
+
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                button1.PerformClick();
+            }
+        }
+    }
+}
